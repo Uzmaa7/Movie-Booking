37e482ce324a0750e2ef222a7cce6bee8f0fa07a
Uzmaa7 <uk9971khan@gmail.com>
2026-02-25 18:11:46 +0530

    feat: add movie schema and body-parser middleware
    Defined Mongoose schema for Movie model
    
    Added express.json and urlencoded middleware to handle request bodies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import swaggerUi from "swagger-ui-express";
+import { specs } from "./config/swagger.js";
+
 dotenv.config({
     path: "./.env"
 })
@@ -9,12 +12,15 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 
 app.get("/", (req, res) => {
     console.log("HEy")
 })
 
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 export default app;
 
---
+++ Backend/src/models/movie.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+
+    description : {
+        type : String,
+        required : true,
+    },
+
+    casts : {
+        type : [String],
+        required: true,
+    },
+
+    trailorUrl: {
+        type : String,
+        required: true,
+    },
+
+    language: {
+        type : String,
+        required: true,
+    },
+
+    releaseDate : {
+        type : String,
+        required: true,
+    },
+
+    director : {
+        type : String,
+        required : true,
+    },
+    
+    releaseStatus : {
+        type : String,
+        required : true,
+        default : "Released",
+    },
+    
+}, {timestamps:true});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+export default Movie;
