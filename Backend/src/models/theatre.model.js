69d857e6adaaf2a598bb1299f4a3b030da89a8db
Uzmaa7 <uk9971khan@gmail.com>
2026-02-27 01:07:56 +0530

    Feat: Added Theatre Schema

---
+++ Backend/src/models/theatre.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const theatreSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+    },
+
+    city :{
+        type: String,
+        required: true,
+    },
+
+    pincode: {
+        type: Number,
+        required: true,
+    },
+    address: String,
+
+}, {timestamps: true});
+
+const Theatre = mongoose.model("Theatre", theatreSchema);
+
+export default Theatre;
