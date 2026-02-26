06cd434481958387c2e7876d7a1926a871d21c11
Uzmaa7 <uk9971khan@gmail.com>
2026-02-26 19:42:00 +0530

    Feat(movie): add id and update validations for get, delete, and update APIs

--- Backend/src/routes/movie.routes.js
+++ Backend/src/routes/movie.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createMovie, deleteMovie, getMovie, updateMovie } from "../controllers/movie.controllers.js";
-import { createMovieValidator } from "../validators/movie.Validators.js";
+import { createMovieValidator , validateMovieId, updateMovieValidator} from "../validators/movie.Validators.js";
 import { validate } from "../middlewares/validator.middleware.js";
 
 
@@ -9,13 +9,13 @@ const movieRouter = express.Router();
 
 movieRouter.post("/create-movie", createMovieValidator(), validate, createMovie);
 
-movieRouter.get("/:id", getMovie);
+movieRouter.get("/:id",validateMovieId(), validate, getMovie);
 
-movieRouter.delete("/:id", deleteMovie);
+movieRouter.delete("/:id",validateMovieId(), validate,  deleteMovie);
 
-movieRouter.put("/:id", updateMovie);
+movieRouter.put("/:id",validateMovieId(), updateMovieValidator, validate,  updateMovie);
 
-movieRouter.patch("/:id", updateMovie);
+movieRouter.patch("/:id",validateMovieId(), updateMovieValidator, validate, updateMovie);
 
 export default movieRouter;
 
--- Backend/src/validators/movie.Validators.js
+++ Backend/src/validators/movie.Validators.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body , param} from "express-validator";
 
 const createMovieValidator = () => {
     return [
@@ -53,4 +53,74 @@ const createMovieValidator = () => {
     ]
 }
 
-export {createMovieValidator};
+export const validateMovieId = () => {
+  return [
+    param("id")
+      .notEmpty().withMessage("Movie ID is required")
+      .isMongoId().withMessage("Invalid Movie ID format"),
+  ];
+};
+
+export const updateMovieValidator = () => {
+     return [
+        body("name")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("The name of the movie is not present in request sent")
+            .isString().withMessage("name must be a string"),
+
+        body("description")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("The description of the movie is not present in request sent")
+            .isLength({ min: 10, max: 30 }).withMessage("the length of description must be between 10 to 30 characters long")
+            .isString().withMessage("description must be a string"),
+
+        body("casts")
+            .optional()
+            .isArray({ min: 1 }).withMessage("Casts must be a non-empty array"),
+
+        //Validate each element inside casts
+        body("casts.*")
+            .optional()
+            .isString().withMessage("Each cast member must be a string")
+            .trim()
+            .notEmpty()
+            .withMessage("Cast name cannot be empty"),
+
+        body("trailorUrl")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("the URL of the movie is missing"),
+            // .isURL().withMessage("The trailer URL must be a valid URL"),
+
+        body("language")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("The language of the movies is not present in request")
+            .isString().withMessage("language must be a string"),
+
+        body("releaseDate")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("Release date is required")
+            // .isISO8601().withMessage("Release date must be a valid date (YYYY-MM-DD)")
+            .isString().withMessage("releaseDate must be a string"),
+
+        body("director")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("director name is required")
+            .isString().withMessage("director must be a string")
+            .isLength({ min: 2 }).withMessage("director name must be at least 2 characters long"),
+
+
+        body("releaseStatus")
+            .optional()
+            .trim()
+            .notEmpty().withMessage("releaseStatus is required")
+            .isIn(["RELEASED", "COMING_SOON", "BLOCKED"]).withMessage("releaseStatus must be RELEASED, COMING_SOON, or BLOCKED"),
+    ]
+}
+
+export {createMovieValidator, validateMovieId, updateMovieValidator};
