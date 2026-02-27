035555f3d3a13a514b20c204bd64457c7a804932
Uzmaa7 <uk9971khan@gmail.com>
2026-02-27 20:53:37 +0530

    Feat: Added reference to the movie collection inside theatre resource

--- Backend/src/models/theatre.model.js
+++ Backend/src/models/theatre.model.js
@@ -21,6 +21,11 @@ const theatreSchema = new mongoose.Schema({
     },
     address: String,
 
+    movies : {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Movie"
+    }
+
 }, {timestamps: true});
 
 const Theatre = mongoose.model("Theatre", theatreSchema);
