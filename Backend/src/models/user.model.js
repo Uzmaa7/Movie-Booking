c853a0417098a732dca8bd2ff0bac20286c08020
Uzmaa7 <uk9971khan@gmail.com>
2026-03-01 14:49:57 +0530

    Feat: Encrypted plain password during register

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -34,6 +35,19 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
+userSchema.pre("save", async function(){
+    //add a check so password is only hashed when modified:
+    //Why this is important?
+    //If you update a user (like changing name), you don't want to hash the already hashed password again.
+    if(!this.isModified("password")) return next();
+
+   const hashedPassword =  await bcrypt.hash(this.password, 10);
+   this.password = hashedPassword;
+   
+})
+
 const User = mongoose.model("User", userSchema);
+
+
 
 export default User;
