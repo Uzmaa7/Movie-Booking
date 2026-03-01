25d8a489b632f3eee6b7f5a3f71619d2cb8a80eb
Uzmaa7 <uk9971khan@gmail.com>
2026-03-02 00:34:05 +0530

    Feat: Added isAdmin authorization check

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,9 +2,10 @@ import express from "express";
 import { updateUserRoleOrStatus } from "../controllers/user.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { updateUserRoleOrStatusValidator } from "../validators/user.Validators.js";
+import { isAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
-userRouter.patch("/:id",updateUserRoleOrStatusValidator(), validate, updateUserRoleOrStatus)
+userRouter.patch("/:id", verifyJWT, isAdmin, updateUserRoleOrStatusValidator(), validate, updateUserRoleOrStatus)
 
 export default userRouter;
