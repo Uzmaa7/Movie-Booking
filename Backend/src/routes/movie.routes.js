892f09c9861afe247e9c0d026afb874b43e50ef3
Uzmaa7 <uk9971khan@gmail.com>
2026-03-27 16:55:49 +0530

    Fix: Added validation to create movie api

--- Backend/src/routes/movie.routes.js
+++ Backend/src/routes/movie.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import { createMovie, deleteMovie, getMovie, getMovies, updateMovie } from "../controllers/movie.controllers.js";
 import { createMovieValidator , validateMovieId, updateMovieValidator} from "../validators/movie.Validators.js";
 import { validate } from "../middlewares/validator.middleware.js";
+import {isAdminOrClient, verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 
 const movieRouter = express.Router();
 
 // CRUD operations for Movie
-movieRouter.post("/create-movie", createMovieValidator(), validate, createMovie);
+movieRouter.post("/create-movie", verifyJWT, isAdminOrClient, createMovieValidator(), validate, createMovie);
 
 movieRouter.get("/:id",validateMovieId(), validate, getMovie);
 
